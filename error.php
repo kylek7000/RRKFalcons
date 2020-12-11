b9ecb1a45ed82bfdb002a704883b49f6cc7d66d9
bigbanda <67397201+bigbanda@users.noreply.github.com>
2020-12-11 10:40:19 -0500

    beautified it

--- error.php
+++ error.php
@@ -1,12 +1,18 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
   <head>
       <title>Error</title>
+      <link href="main.css" type="text/css" rel="stylesheet">
   </head>
   <body>
+    <div class="header">
+      <h1>Time Clock</h1>
+    </div>
+    <div id="wrap">
       <h1>Error</h1>
-      <p>The username or password is inccorect!</p>
+      <p>The username or password is incorrect!</p>
       <p>Please Try Again:</p>
       <a href="index.php">Log In</a>
+    </div>
   </body>
 </html>
