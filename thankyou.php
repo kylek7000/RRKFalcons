066bf969f6a28d8dbb7887736608d76947d0536b
bigbanda <67397201+bigbanda@users.noreply.github.com>
2020-12-11 10:41:11 -0500

    beautified it

--- thankyou.php
+++ thankyou.php
@@ -1,11 +1,17 @@
 <!DOCTYPE html>
-<html>
+<html lang = "en">
   <head>
       <title>Done</title>
+      <link href="main.css" type="text/css" rel="stylesheet">
   </head>
   <body>
+    <div class="header">
+      <h1>Time Clock</h1>
+    </div>
+    <div id="wrap">
       <h1>Done</h1>
       <p>Thank you! Your data has been sumbitted!</p>
       <a href="index.php">Log In</a>
+    </div>
   </body>
 </html>
